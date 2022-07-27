01c167aa3df38945c04b0df6cf9c91f627f6dc8e
about-joo91 <about.joo91@gmail.com>
2022-07-27 11:17:01 +0900

    네트워크 코드 더 간결하게 수정

--- 7_27/pro-network.py
+++ 7_27/pro-network.py
@@ -1,23 +1,17 @@
-
-def dfs(visited, graph, node):
+def dfs(visited, computers, node):
     if visited[node] == 0:
         visited[node] = 1
-        for next_node in graph[node]:
-            if visited[next_node] == 0:
-                dfs(visited, graph, next_node)
+        for idx, is_network in enumerate(computers[node]):
+            if visited[idx] == 0 and is_network:
+                dfs(visited, computers, idx)
         return True
-    
+    return False
 
 def solution(n, computers):
-    graph = [[] for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if computers[i][j] == 1 and i != j:
-                graph[i].append(j)
     cnt = 0
     visited = [0] * n
     for i in range(n):
-        if dfs(visited, graph, i):
+        if dfs(visited, computers, i):
             cnt+=1
     return cnt
 
