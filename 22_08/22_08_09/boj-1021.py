5df57b39ac568ffd3599356b983fb153c01ffa54
about-joo91 <about.joo91@gmail.com>
2022-08-10 10:09:28 +0900

    22-08-10 알고리즘

--- 22_08/22_08_09/boj-1021.py
+++ 22_08/22_08_09/boj-1021.py
@@ -2,6 +2,21 @@ import sys
 from collections import deque
 
 cnt = 0
-N, M = map(int, sys.stdin.readline())
+N, M = map(int, sys.stdin.readline().split())
 queue = deque(list(range(1,N+1)))
-print(queue.index(2))
+nums_to_find = list(map(int, sys.stdin.readline().split()))
+for num in nums_to_find:
+    if queue[0] == num:
+        queue.popleft()
+        continue
+    idx = queue.index(num)
+    minus_idx = len(queue) - idx
+    if idx < minus_idx:
+        queue.rotate(-1*idx)
+        queue.popleft()
+        cnt+= idx
+    else:
+        queue.rotate(minus_idx)
+        queue.popleft()
+        cnt+= minus_idx
+print(cnt)
---
+++ 22_08/22_08_10/boj-18258.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+
+iter_num = int(sys.stdin.readline())
+queue = deque()
+for _ in range(iter_num):
+    operator = sys.stdin.readline().split(' ')
+    if operator[0] == 'push':
+        queue.append(operator[1])
+    elif operator[0] == 'size':
+        print(len(queue))
+    elif operator[0] == 'empty':
+        if queue: print(0)
+        else: print(1)
+    elif operator[0] == 'front':
+        if queue: print(queue[0])
+        else: print(-1)
+    elif operator[0] =='pop':
+        if queue: print(queue.popleft())
+        else: print(-1)
+    elif operator[0] == 'back':
+        if queue: print(queue[-1])
+        else: print(-1)
