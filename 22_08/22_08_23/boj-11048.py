99576160b917467b5ea9b0e3914d4bea6b018cf1
about-joo91 <about.joo91@gmail.com>
2022-08-23 08:57:32 +0900

    8월 23일 알고리즘 추가

--- 22_08/22_08_23/boj-11048.py
+++ 22_08/22_08_23/boj-11048.py
@@ -1,1 +1,10 @@
-print(1+ 9+ 8+ 7+ 6)
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+dp = [[0] * (M+1) for _ in range(N+1)]
+candy_map = [list(map(int, input().split())) for _ in range(N)]
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        dp[i][j] = candy_map[i-1][j-1] + max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+        
+print(dp[N][M])
