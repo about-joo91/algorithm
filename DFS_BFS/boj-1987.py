eadb3e938b2a3b6de7b3272fe2732a833fe90023
about-joo91 <about.joo91@gmail.com>
2022-12-15 23:54:29 +0900

    수정버전

--- DFS_BFS/boj-1987.py
+++ DFS_BFS/boj-1987.py
@@ -1,14 +1,12 @@
 import sys
-from collections import defaultdict
 input = sys.stdin.readline
 
 R, C = map(int, input().split())
 maps = [ list(input()) for _ in range(R)]
-visited = [[False] *C for _ in range(R)]
+
 directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
 
-
-used = defaultdict(str)
+used = set()
 answer = 0
 
 def dfs(row, col):
@@ -19,14 +17,11 @@ def dfs(row, col):
         next_row = row + direction[0]
         next_col = col + direction[1]
         if 0 > next_row or next_row >= R or 0 > next_col or next_col >= C: continue
-        if visited[next_row][next_col]: continue
+        if maps[next_row][next_col] in used: continue
+        used.add(maps[next_row][next_col])
+        dfs(next_row, next_col)
+        used.remove(maps[next_row][next_col])
 
-        if maps[next_row][next_col] not in used:
-            used[maps[next_row][next_col]]
-            dfs(next_row, next_col)
-            del used[maps[next_row][next_col]]
-
-visited[0][0] = True
-used[maps[0][0]] = 1
+used.add(maps[0][0])
 dfs(0, 0)
 print(answer)
