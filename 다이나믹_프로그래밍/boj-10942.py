018edc0da31e0fa8db35e94e819708c9804985a9
about-joo91 <about.joo91@gmail.com>
2023-01-25 22:19:00 +0900

    1월 25일 알고리즘

---
+++ 다이나믹_프로그래밍/boj-10942.py
@@ -0,0 +1,32 @@
+def get_is_palindrome_of_whole_num_of_cases():
+    for i in range(N):
+        left = right = i
+        while left >= 0 and right < N:
+            if numbers[left] == numbers[right]: is_pal[left][right] = 1
+            else: break
+            left -= 1
+            right += 1
+        left = i
+        right = i+1
+        while left >= 0 and right < N:
+            if numbers[left] == numbers[right]: is_pal[left][right] = 1
+            else: break
+            left -=1
+            right+=1
+    
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+    
+    N = int(input())
+    numbers = list(map(int, input().split()))
+    M = int(input())
+
+    is_pal = [[0] * N for _ in range(N)]
+    get_is_palindrome_of_whole_num_of_cases()
+    
+    for _ in range(M):
+        start, end = map(int, input().split())
+        print(is_pal[start-1][end-1])
+
