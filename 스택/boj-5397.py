cb3fc2e3338585e46dcbf1d5d9617e7fb266f3f3
about-joo91 <about.joo91@gmail.com>
2023-01-31 22:41:52 +0900

    파일 위치 수정

---
+++ 스택/boj-5397.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+for _ in range(N):
+    left = []
+    right = []
+    kang_input = list(input())
+
+    for char in kang_input:
+        if char == "<":
+            if left: right.append(left.pop())
+        elif char == ">":
+            if right: left.append(right.pop())
+        elif char == "-":
+            if left: left.pop()
+        else:
+            left.append(char)
+
+    print("".join(left)+ "".join(reversed(right)))
