9e7dcf1b7e83400ae22e6033d757204c10f47763
about-joo91 <about.joo91@gmail.com>
2023-02-01 23:54:43 +0900

    로직 수정

--- 다이나믹_프로그래밍/boj-9252.py
+++ 다이나믹_프로그래밍/boj-9252.py
@@ -1,21 +1,34 @@
-fir_str = input()
-sec_str = input()
+import sys
+input = sys.stdin.readline
 
+fir_str = " " + input().rstrip()
+sec_str = " " + input().rstrip()
 N = len(fir_str)
 M = len(sec_str)
 
-lcs = [[""] * (M+1) for _ in range(N+1)]
-dp = []
-max_value = 0
+lcs = [[0] * M for _ in range(N)]
 
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        if fir_str[i-1] == sec_str[j-1]:
-            lcs[i][j] = lcs[i-1][j-1] + fir_str[i-1]
+for i in range(1, N):
+    for j in range(1, M):
+        if fir_str[i] == sec_str[j]:
+            lcs[i][j] = lcs[i-1][j-1] + 1
         else:
-            if len(lcs[i-1][j]) > len(lcs[i][j-1]):
-                lcs[i][j] = lcs[i-1][j]
-            else: lcs[i][j] = lcs[i][j-1]
+            lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
 
-print(len(lcs[N][M]))
-print("".join(lcs[N][M]))
+print(lcs[N-1][M-1])
+
+answer = []
+row = N-1
+col = M-1
+
+while row > 0 and col > 0:
+    if lcs[row][col-1] == lcs[row][col]:
+        col -=1
+    elif lcs[row-1][col] == lcs[row][col]:
+        row -=1
+    else:
+        answer.append(fir_str[row])
+        row-=1
+        col-=1
+
+print("".join(answer[::-1]))
