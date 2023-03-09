6aac43a4c5b05ac18397f3506a78d124b85996bb
about-joo91 <about.joo91@gmail.com>
2023-03-09 23:02:47 +0900

    3월 8일 알고리즘

---
+++ 스택/boj-14719.py
@@ -0,0 +1,17 @@
+H, W = map(int, input().split())
+cities = list(map(int, input().split()))
+
+stack = []
+answer = 0
+
+for idx in range(W):
+    while stack and cities[stack[-1]] < cities[idx]:
+        print(stack)
+        prev_city = stack.pop()
+        if not stack: break
+        distance = (idx - stack[-1]) - 1
+        height = min(cities[stack[-1]], cities[idx]) - cities[prev_city]
+        answer += (height * distance)
+    stack.append(idx)
+
+print(answer)
