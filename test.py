f990bbc172265418dc416fcae9235ed270498742
about-joo91 <about.joo91@gmail.com>
2023-03-17 19:36:12 +0900

    3월 17일 알고리즘

--- test.py
+++ test.py
@@ -1,17 +1,2 @@
 import sys
 sys.stdin = open('test.txt','r')
-
-N = int(input())
-jump_numbers = [0] + list(map(int, input().split()))
-INF = int(10e9)
-dp = [INF] * (N+1)
-dp[1] = 0
-
-for i in range(1, N+1):
-    for j in range(1, jump_numbers[i]+1):
-        if i + j > N: continue
-        dp[i+j] = min(dp[i] + 1, dp[i+j])
-
-if dp[-1] == INF:
-    print(-1)
-else: print(dp[-1])
