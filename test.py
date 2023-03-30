420600060058c9ae59d71e0d8b819b81a20db91f
about-joo91 <about.joo91@gmail.com>
2023-03-30 12:02:52 +0900

    3월 30일 알고리즘

--- test.py
+++ test.py
@@ -1,24 +1,3 @@
 import sys
 sys.stdin = open('test.txt','r')
 
-def get_count_of_armor():
-    left = 0
-    right = len(materials)-1
-
-    cnt = 0
-    while left < right:
-        sum_of_material = materials[left] + materials[right]
-        if sum_of_material == target:
-            cnt+=1
-        if sum_of_material < target:
-            left+=1
-        else:
-            right-=1
-    return cnt
-
-N = int(input())
-target = int(input())
-materials = sorted(list(map(int, input().split())))
-
-count_of_armor = get_count_of_armor()
-print(count_of_armor)
