6e204a4066eaccd088e76138520a706809913c42
about-joo91 <about.joo91@gmail.com>
2023-04-20 21:26:08 +0900

    4월 20일 알고리즘

--- 정렬/boj-2170.py
+++ 정렬/boj-2170.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdin.readline
+
 N = int(input())
 lines = sorted([tuple(map(int, input().split())) for _ in range(N)])
 
@@ -7,7 +10,6 @@ answer = 0
 
 for i in range(1, N):
     cur_start, cur_end = lines[i]
-    print(cur_start, cur_end)
 
     if end > cur_start:
         end = max(cur_end, end)
