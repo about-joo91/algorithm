648821df72f8e02fea7cebb87e15f275cf6e4185
about-joo91 <about.joo91@gmail.com>
2023-05-23 23:25:40 +0900

    5월 23일 알고리즘

--- test.py
+++ test.py
@@ -1,24 +1,2 @@
 import sys
 sys.stdin = open('test.txt','r')
-
-def delete_node(target):
-    nodes[target] = -INF
-    for idx in range(N):
-        if target == nodes[idx]:
-            delete_node(idx)
-
-
-if __name__ == "__main__":
-    N = int(input())
-    nodes = list(map(int, input().split()))
-    target_node = int(input())
-    result = 0
-    INF = 2
-
-    delete_node(target_node)
-
-    for i in range(N):
-        if nodes[i] != -INF and i not in nodes:
-            result+=1
-
-    print(result)
