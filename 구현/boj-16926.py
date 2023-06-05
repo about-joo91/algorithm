6493649ba085e6c70090af18669f352ba7007eb1
about.joo91 <55477835+about-joo91@users.noreply.github.com>
2023-06-06 00:56:25 +0900

    풀이 수정

--- 구현/boj-16926.py
+++ 구현/boj-16926.py
@@ -1,39 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-N, M, R = map(int, input().split())
-arr = []
-for i in range(N):
-    arr.append(list(map(int, input().split())))
+N, M, R = map(int, input().rstrip().split())
+arr = [list(map(int, input().rstrip().split())) for _ in range(N)]
 
 for _ in range(R):
-    for i in range(min(N, M) // 2):
-        x, y = i, i
-        value = arr[x][y]
+	for i in range(min(N, M) // 2):
+		fir_value = arr[i][i]
 
-        for j in range(i + 1, N - i):
-            x = j
-            prev_value = arr[x][y]
-            arr[x][y] = value
-            value = prev_value
+		for j in range(i + 1, M - i):
+			arr[i][j-1] = arr[i][j]
+		cur_col = j
+			
+		for j in range(i + 1, N - i):
+			arr[j-1][cur_col] = arr[j][cur_col]
+		cur_row = j
 
-        for j in range(i + 1, M - i):
-            y = j
-            prev_value = arr[x][y]
-            arr[x][y] = value
-            value = prev_value
+		for j in range(M-i-1, i, -1):
+			arr[cur_row][j] = arr[cur_row][j-1]
+		cur_col = i
+		
+		for j in range(N-i-1, i, -1):
+			arr[j][cur_col] = arr[j-1][cur_col]
+		arr[i+1][i] = fir_value
 
-        for j in range(i + 1, N - i):  # 우
-            x = N - j - 1
-            prev_value = arr[x][y]
-            arr[x][y] = value
-            value = prev_value
-
-        for j in range(i + 1, M - i):
-            y = M - j - 1
-            prev_value = arr[x][y]
-            arr[x][y] = value
-            value = prev_value
-
-for i in arr:
-    print(*i)
+for i in range(N):
+	print(*arr[i])
