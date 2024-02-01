c1d0e9919401bc9773a0bbc10a3932459e2992d1
about.joo91 <55477835+about-joo91@users.noreply.github.com>
2024-02-02 08:06:38 +0900

    2024_02_02
    ### DESC
    - 같은 트리인지 검사하는 문제
    - 트리를 타고 내려가면서 value값을 검사하여 문제를 해결함
    - 따지고 보면 DFS로 문제를 풀었다고 할 수 있을 것 같다.
    - 재귀 함수로 p가 none, q가 none일 때까지 검사하고 둘다 None일 때까지 내려갔다면 True, 둘 중 하나가 none라면 같은 트리가 아니므로 False를 반환한다.
    - p, q의 각 밸류가 같이 않아도 같은 트리가 아니므로 False를 반환한다.
    - 왼쪽 검사, 오른쪽 검사를 재귀로 실행해 and 연산자로 묶어 결과를 반환하면 최종적으로 모든 노드를 확인한 isSameTree값을 알 수 있다.
    
    ### FEEDBACK
    - 정답을 풀기위한 재귀함수를 금세 떠올릴 수 있었다.
    - 그러나 두번째 정답처럼 더 깔끔한 답을 떠올리지는 못했다. 더 깔끔하고 보기 편한 코드를 쓰기위해 노력하자

---
+++ 이진검색트리/leet_code_same_tree.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == None and q == None:
+            return True
+        elif p == None or q == None:
+            return False
+
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        elif not p or not q or p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
